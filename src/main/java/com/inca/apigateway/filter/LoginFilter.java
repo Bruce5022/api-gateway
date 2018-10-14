7b744a1383b8106b94d134f127bc63c264328310
史战伟 <shizhanwei123@126.com>
2018-10-15 07:39:12 +0800

    网关增加过来器

---
+++ src/main/java/com/inca/apigateway/filter/LoginFilter.java
@@ -0,0 +1,41 @@
+package com.inca.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+@Component
+public class LoginFilter extends ZuulFilter {
+
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+
+        System.out.println("请求URI:" + request.getRequestURI());
+        System.out.println("请求URL:" + request.getRequestURL());
+
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
